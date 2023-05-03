13cc5549db80b71e66d275a28622b98f21d04f8e
yakuza <rinat.kuchukov@gmail.com>
2023-05-03 09:27:09 +0600

    Update checksum.py

--- checksum.py
+++ checksum.py
@@ -1,7 +1,12 @@
+import sys
 import struct
 import binascii
 
-filename = input("Введите имя файла: ")
+if len(sys.argv) != 2:
+    print("Ошибка: укажите файл")
+    sys.exit(1)
+
+filename = sys.argv[1]
 
 ranges = [(0x0000, 0xFEFF), (0x10000, 0x1FEFF)]
 
@@ -15,13 +20,23 @@ for start, end in ranges:
             crc += byte
             byte_str = struct.pack('>H', crc & 0xFFFF)
 
-        print(f"  CRC: {binascii.hexlify(byte_str).decode('utf-8')}")
+        current_crc = binascii.hexlify(byte_str).decode('utf-8')
+        print(f"  CRC: {current_crc}")
 
-    answer = input("Записать CRC? (y/n): ")
-    if answer == "y":
-        with open(filename, "r+b") as f:
-            f.seek(0xFF00 if start == 0x0000 else 0x1FF00)
-            f.write(byte_str)
-            print("Успешно")
+    with open(filename, "rb") as f:
+        f.seek(0xFF00 if start == 0x0000 else 0x1FF00)
+        saved_crc = f.read(2)
+
+    if saved_crc == byte_str:
+        print("  Текущее значение CRC совпадает с сохраненным значением.")
     else:
-        print("Неудачно")
+        current_crc = binascii.hexlify(saved_crc).decode('utf-8')
+        print(f"  Текущее значение CRC: {current_crc}")
+        answer = input("  Записать CRC? (y/n): ")
+        if answer == "y":
+            with open(filename, "r+b") as f:
+                f.seek(0xFF00 if start == 0x0000 else 0x1FF00)
+                f.write(byte_str)
+                print("  Успешно записано новое значение CRC.")
+        else:
+            print("  Не записано новое значение CRC.")
